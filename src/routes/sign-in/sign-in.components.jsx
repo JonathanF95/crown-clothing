ffa4a2892135ae738ae46d42ca82b47116770d06
Jonathan Fuentes <john.fuen95@gmail.com>
2024-06-19 22:47:23 -0500

    Add files via upload

--- src/routes/sign-in/sign-in.components.jsx
+++ src/routes/sign-in/sign-in.components.jsx
@@ -1,6 +1,7 @@
 import { auth, signInWithGooglePopup, createUserDocumentFromAuth, signInWithGoogleRedirect } from '../../utils/firebase/firebase.utils'
 import { useEffect } from 'react';
 import { getRedirectResult } from 'firebase/auth';
+import SignUpForm from '../../components/sign-up-form/sign-up-form.component';
 
 const SignIn = () => {
     useEffect(() => {
@@ -30,6 +31,7 @@ const SignIn = () => {
             <h1>Sign In Page</h1>
             <button onClick={logGoogleUser}>Sign in with Google Popup</button>
             <button onClick={signInWithGoogleRedirect}>Sign in with Google Redirect</button>
+            <SignUpForm />
         </div>
     );
 };
